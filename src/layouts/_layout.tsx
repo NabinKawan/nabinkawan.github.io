67c4641f43e2b232703f97d106ab9da9f1d27713
KNabin <nabin.kawan.9@gmail.com>
2023-07-13 19:28:51 +0545

    Initialize project

---
+++ src/layouts/_layout.tsx
@@ -0,0 +1,13 @@
+/**
+ * This component is intended for internal use only.
+ * 
+ * Renders the layout component.
+ */
+
+import React from 'react'
+
+export default function Layout() {
+  return (
+    <div>Layout</div>
+  )
+}
