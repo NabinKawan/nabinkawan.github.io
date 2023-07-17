b148b21330e4b7aa1f97e1228dd9a6afe5abbd9a
KNabin <nabin.kawan.9@gmail.com>
2023-07-17 07:23:06 +0545

    Handle contact me button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,14 @@ import { useEffect, useState } from 'react';
 export default function Home() {
     const [portfolio, setPortfolio] = useState<PortfolioDto | null>(null);
 
+    const _handleContactMe = () => {
+        const recipient = 'nabin.kawan.9@gmail.com';
+        const subject = 'Regarding Contact';
+
+        const mailtoUrl = `https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=${recipient}&su=${subject}`;
+        window.open(mailtoUrl);
+    };
+
     useEffect(() => {
         getPortfolio().then((data) => {
             if (data) {
@@ -28,11 +36,9 @@ export default function Home() {
             <div className="w-full">
                 <Header portfolio={portfolio} className="absolute top-0" />
 
-                <AnchorLink href={`mail-to:${portfolio.profile.email}`} key="contact-me">
-                    <Button color="secondary" className="fixed right-10 z-50 mt-3 lg:right-20 xl:right-44">
-                        Contact Me
-                    </Button>
-                </AnchorLink>
+                <Button color="secondary" className="fixed right-10 z-50 mt-3 lg:right-20 xl:right-44" onClick={_handleContactMe}>
+                    Contact Me
+                </Button>
 
                 <Banner portfolio={portfolio} className="pl-6 md:pl-10  lg:pl-20 xl:pl-40" />
                 <div className="space-y-6 px-6 md:px-10 lg:px-20 xl:px-40">
